ba5fa9172c43fc8b55d03555ad712e1674540847
Linh. Vo Van - CMC Global DU3.11 <vvlinh4@cmcglobal.vn>
2022-04-26 17:47:17 +0700

    build database and setup prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { AuthModule } from './auth/auth.module';
 // import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { BookbarkModule } from './bookbark/bookbark.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookbarkModule],
+  imports: [AuthModule, UserModule, BookbarkModule, PrismaModule],
 })
 export class AppModule {}

ae407322b6fe7dee219951f73b096445d4e45d02
Linh. Vo Van - CMC Global DU3.11 <vvlinh4@cmcglobal.vn>
2022-04-26 15:05:25 +0700

    initilizing the base project and get the base knowledge

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { BookbarkModule } from './bookbark/bookbark.module';
+
+@Module({
+  imports: [AuthModule, UserModule, BookbarkModule],
+})
+export class AppModule {}

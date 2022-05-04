edbef1b4a77ec4a88428a9b7ada86d0443a9b1f9
Linh. Vo Van - CMC Global DU3.11 <vvlinh4@cmcglobal.vn>
2022-05-04 15:16:17 +0700

    nest guard & customer decorator to get user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
   @Get('me')
-  getMe() {
-    return 'LINHK';
+  getMe(@GetUser() user: User) {
+    return user;
+  }
+
+  @Get('myemail')
+  getMyEmail(@GetUser('email') email: string): string {
+    return email;
+  }
+
+  @Patch()
+  editUser() {
+    return 'a';
   }
 }
